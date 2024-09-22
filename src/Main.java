c7a51b7c5e093918b843daf176502d2e94e1f81f
Yilikal Ayalew <yilikalayalew76@gmail.com>
2024-09-22 16:38:00 +0300

    Initial Commit of Code Challenge 37/Pool Area

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
+    }
+}
